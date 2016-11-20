c53fa314500418e215204282644f6bc8f79af812
Rossella <corso@10.0.1.196>
2016-11-20 18:44:15 +0100

    Client

---
+++ WebServiceProgetto/src/main/java/it/alfasoft/rossella/model/BustaPaga.java
@@ -0,0 +1,11 @@
+package it.alfasoft.rossella.model;
+
+public class BustaPaga {
+	
+	private long id_busta;
+	
+	private Dipendente dipendente;
+	private String dataEmissione;
+	private double importo;
+
+}
